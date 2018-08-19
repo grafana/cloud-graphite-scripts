0b07e4d18e9b5960aa7a99b836cd66b19873f73d
Dieter Plaetinck <dieter@grafana.com>
2018-08-19 15:42:17 +0200

    add shell/curl example

---
+++ main.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+key="<api-key>"
+addr="https://<your-instance>.hosted-metrics.grafana.net"
+
+curl -X POST -H "Authorization: Bearer $key" -H "Content-Type: application/json" "$addr/metrics" -d '[{
+    "name": "test.metric",
+    "interval": 10,
+    "value": 12.345,
+    "time": 1534685580
+},
+{
+    "name": "test.metric",
+    "interval": 10,
+    "value": 12.345,
+    "time": 1534685590
+    }
+]'
